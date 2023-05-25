0b9f8869df8ff1608ab08f7cc8b160d3c9c15818
TuralFR <124531378+TuralFR@users.noreply.github.com>
2023-05-25 21:18:33 +0300

    hash_scanner.py

--- hash_scanner.py
+++ hash_scanner.py
@@ -9,49 +9,49 @@ banner = """
 """
 print("[md5-sha1]")
 print("-----------------------------")
-print("[?]LUTFEN BIR FORMAT GIRINIZ: ")
+print("PLEASE ENTER A FORMAT: ")
 print("-----------------------------")
-format = input(">>>[+]FORMAT GIRINIZ / [MD5]-[SHA1]: ")
+format = input(">>>ENTER A FORMAT / [MD5]-[SHA1]: ")
 
 if format =="md5" or format == "MD5":
 
     print("-----------------------------")
-    print("[?]LUTFEN BIR HASH GIRINIZ: ")
+    print("PLEASE ENTER A HASH: ")
     print("-----------------------------")
-    md5 = input(">>>[+]MD5 HASH: ")
+    md5 = input(">>>MD5 HASH: ")
     print("\n")
-    sorgula = "https://md5.gromweb.com/?string={}".format(md5)
-    istek_yolla = requests.get(sorgula)
+    question = "https://md5.gromweb.com/?string={}".format(md5)
+    send_request = requests.get(question)
     sleep(3)
-    soup = BeautifulSoup(istek_yolla.content, "lxml")
-    hash_bilgisi = soup.find_all("em",attrs={"class":"long-content string"})
+    soup = BeautifulSoup(send_request.content, "lxml")
+    hash_info = soup.find_all("em",attrs={"class":"long-content string"})
 
-    for hash in hash_bilgisi:
+    for hash in hash_info:
 
-        print("[?]SEARCH HASH: {}".format(md5))
+        print("SEARCH HASH: {}".format(md5))
         print("-----------------------------")
-        print("[%]HASH: ".format(hash.txt))
+        print("HASH: ".format(hash.txt))
         print("-----------------------------")
 
 elif format =="sha1" or format == "SHA1":
 
     print("-----------------------------")
-    print("[?]LUTFEN BIR SHA1 GIRINIZ: ")
+    print("PLEASE ENTER A SHA1: ")
     print("-----------------------------")
-    md5 = input(">>>[+]SHA1 HASH: ")
+    md5 = input(">>>SHA1 HASH: ")
     print("\n")
-    sorgula = "https://sha1.gromweb.com/?string={}".format("sha1")
+    question = "https://sha1.gromweb.com/?string={}".format("sha1")
 
-    istek_yolla = requests.get(sorgula)
+    send_request = requests.get(question)
     sleep(3)
-    soup = BeautifulSoup(istek_yolla.content, "lxml")
-    hash_bilgisi = soup.find_all("em",attrs={"class":"long-content string"})
+    soup = BeautifulSoup(send_request.content, "lxml")
+    hash_info = soup.find_all("em",attrs={"class":"long-content string"})
 
-    for hash in hash_bilgisi:
+    for hash in hash_info:
 
-        print("[?]SEARCH HASH: {}".format("sha1"))
+        print("SEARCH HASH: {}".format("sha1"))
         print("-----------------------------")
-        print("[%]HASH: ".format(hash.txt))
+        print("SEARCH HASH: ".format(hash.txt))
         print("-----------------------------")
 
 
